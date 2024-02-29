ab91c3a52641f66e25f76c9c4554d5c690ba44d7
Vitor Rafael <rqfvitor@gmail.com>
2024-02-29 18:48:36 -0300

    add sidebar styling

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,22 @@
+import { RiHome5Line } from "react-icons/ri";
+import { RxDashboard } from "react-icons/rx";
 import { SidebarButton } from "../SidebarButton";
 
 export function Sidebar() {
   return (
-    <aside className="w-60 bg-zinc-800">
+    <aside className="w-60 bg-zinc-800 p-3">
       <nav>
-        <ul>
-          <SidebarButton title="Home" path="/" />
-          <SidebarButton title="Dashboard" path="/dashboard" />
+        <ul className="flex flex-col gap-2">
+          <SidebarButton
+            title="Home"
+            path="/"
+            icon={<RiHome5Line size={24} />}
+          />
+          <SidebarButton
+            title="Dashboard"
+            path="/dashboard"
+            icon={<RxDashboard size={24} />}
+          />
         </ul>
       </nav>
     </aside>
--- src/components/SidebarButton/index.tsx
+++ src/components/SidebarButton/index.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export function SidebarButton({
   title,
   path,
+  icon,
 }: {
   title: string;
   path: string;
+  icon: ReactNode;
 }) {
   const [active, setActive] = useState(false);
   useEffect(() => {
@@ -15,11 +17,18 @@ export function SidebarButton({
   return (
     <li>
       <button
-        className={`w-full h-10 flex items-center px-2 bg-zinc-800  hover:bg-zinc-700 transition-colors duration-200 text-white ${
+        className={`w-full h-10 flex items-center px-2 rounded-md bg-zinc-800  hover:bg-zinc-700 transition-colors duration-200 text-white ${
           active ? "bg-emerald-700 hover:bg-emerald-800" : ""
         }`}
       >
-        {title}
+        {icon && (
+          <span className={`mr-3 ${active ? "text-white" : "text-zinc-500"}`}>
+            {icon}
+          </span>
+        )}
+        <span className="leading-none font-semibold text-zinc-200">
+          {title}
+        </span>
       </button>
     </li>
   );
